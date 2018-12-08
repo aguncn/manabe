17e97ae847fe6f6588a5de1f8ff88be222623dcb
aguncn <aguncn@163.com>
2018-12-08 10:12:11 +0800

    完成rest api测试.
    Signed-off-by: aguncn <aguncn@163.com>

---
+++ manabe/api/tests/test_views.py
@@ -0,0 +1,70 @@
+from django.test import TestCase
+from django.urls import resolve, reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from api.views import UserViewSet
+from api.views import AppViewSet
+from appinput.models import App
+from envx.models import Env
+
+from model_mommy import mommy
+
+
+class UserListTests(TestCase):
+    def setUp(self):
+        self.fatory = APIRequestFactory()
+
+    def test_user_list_view_status_code(self):
+        url = reverse('api:user-list')
+        request = self.fatory.get(url)
+        response = UserViewSet.as_view(({'get': 'list'}))(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_list_url_resolves_user_view(self):
+        view = resolve('/api/users/')
+        self.assertEqual(view.func.__name__, UserViewSet.as_view(({'get': 'list'})).__name__)
+
+
+class AppListTests(TestCase):
+    def setUp(self):
+        self.fatory = APIRequestFactory()
+
+    def test_app_list_view_status_code(self):
+        url = reverse('api:app-list')
+        request = self.fatory.get(url)
+        response = AppViewSet.as_view(({'get': 'list'}))(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_app_list_url_resolves_app_view(self):
+        view = resolve('/api/apps/')
+        self.assertEqual(view.func.__name__, AppViewSet.as_view(({'get': 'list'})).__name__)
+
+
+class ServerCreateTests(TestCase):
+    def setUp(self):
+        self.new_app = mommy.make(App,
+                                  name='ZEP-BACKEND-NODEJS')
+        self.new_env = mommy.make(Env,
+                                  name='TEST')
+        self.user = User.objects.create_user(
+            username='test',
+            email='test@example.com',
+            password='test', )
+        token = Token.objects.get(user__username='test')
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    def test_server_create_view(self):
+        url = reverse('api:server-list')
+        payload = {'name': "192.168.1.213_8888",
+                   'ip_address': "192.168.1.212",
+                   'port': "8888",
+                   'salt_name': "192.168.1.213_8888",
+                   'app_name': "ZEP-BACKEND-NODEJS",
+                   'env_name': "TEST",
+                   'app_user': "root"
+                   }
+        response = self.client.post(url, payload, format='json')
+        self.assertEqual(response.status_code, 201)
